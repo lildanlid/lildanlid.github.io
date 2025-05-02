3f7d9f6d9a51a8f83477c7d22a05594ea8b6d315
lildanlid <144184259+lildanlid@users.noreply.github.com>
2025-05-02 13:00:17 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const content = document.getElementById("content");
+
+    // Add the "loaded" class to trigger the fade-in effect
+    content.classList.add("loaded");
+});
