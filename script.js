be791fd75e18e831f3fa81b5256b0f0fdc77a99c
lildanlid <144184259+lildanlid@users.noreply.github.com>
2025-05-02 13:33:07 +1200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const content = document.getElementById("content");
-
-    // Add the "loaded" class to trigger the fade-in effect
-    content.classList.add("loaded");
-});
